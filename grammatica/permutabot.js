8a9a0109212d7bef59cf86e5d140168769f0edca
eliaspallanzani <38375181+eliaspallanzani@users.noreply.github.com>
2023-02-14 13:58:31 +0100

    Update permutabot.js

--- grammatica/permutabot.js
+++ grammatica/permutabot.js
@@ -1,36 +1,37 @@
     const grammar = `
 
-I ::=   "title:    PermutaBot \\n"
-      ^ "author:   fb.me/atpsd - archivefever.tk@gmail.com \\n"
-      ^ "language: italian \\n"
-      ^ "status:   refinable \\n"
-      ^ "topic:    politics \\n"
-      ^ "comment:  permutatore di affermazione sincera \\n"
-      ^ "warning:  in direzione ostinata e contraria \\n"
+nes (29 sloc)  792 Bytes
+I ::=   "title:    PermutaBot \n"
+      ^ "author:   fb.me/atpsd - archivefever.tk@gmail.com \n"
+      ^ "language: italian \n"
+      ^ "status:   refinable \n"
+      ^ "topic:    politics \n"
+      ^ "comment:  permutatore di prova \n"
+      ^ "warning:  in direzione ostinata e contraria \n"
       ^ "created:  12/02/2023"
 ;
 
 
-S ::= \\ 
+S ::= \ 
       (
             (
-                  il fascismo "<br>" non "&egrave" 
-                  ( un'opinione ^"," "<br>" "&egrave" un reato
-                  | un reato ^"," "<br>" "&egrave" un'opinione )
+                  il mattino 
+                  ( ha l'oro in bocca
+                  | alloro in bocca )
             )
 
             |
 
-            (     il reato "<br>" non "&egrave"
-                  ( un'opinione ^"," "<br>" "&egrave" un fascismo
-                  | un fascismo ^"," "<br>" "&egrave" un'opinione )
+            (     l'oro 
+                  ( ha il mattino in bocca
+                  | in bocca ^"," al mattino )
             )
 
             |
 
-            (      l'opinione "<br>" non "&egrave" 
-                  ( un fascismo ^"," "<br>" "&egrave" un reato
-                  | un reato ^"," "<br>" "&egrave" un fascismo )
+            (      la bocca 
+                  ( ah ^"," l'ora ^"..." 
+                  | ha il mattino nell'ora )
             )
       )
       ^"."
