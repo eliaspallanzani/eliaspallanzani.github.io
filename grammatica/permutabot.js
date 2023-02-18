9c23e939afc08a01950554e374e8c808744328f9
eliaspallanzani <38375181+eliaspallanzani@users.noreply.github.com>
2023-02-18 12:17:47 +0100

    Update permutabot.js

--- grammatica/permutabot.js
+++ grammatica/permutabot.js
@@ -9,7 +9,7 @@ I ::=   "title:    genera\\n"
       ^ "created:  21/04/2018"
 ;
 
-S ::= \\ Titolo  "<br>"
+S ::= \\ "<b>" Titolo "</b> <br>"
       Intro  "<br>" "<br>"
       Nome "-" Razza ^"," Classe ^"," Allineamento Allineamento "</br>"
       le tue caratteristiche sono ^":"  "</br>"
@@ -92,7 +92,7 @@ Opzione2 ::= Pzione2
 Opzione3 ::= Pzione3
 ;
 
-Pzione1 ::= (ma che schifo ^"!" voglio "<a href=https://""&quot"www"."google"."com"/""&quot"">" ritirare "</a>"
+Pzione1 ::= (ma che schifo ^"!" voglio "<a href=https:""&#47""&#47""&quot"www"."google"."com"&#47""&quot"">" ritirare "</a>"
             | preferivo un nano mezzo pitbull ^"," riprova
             | questo allineamento e' contro la mia religione ^"," cambiamo
             | non sono mai stato cosi' offeso ^"," rifammi ^"!"
