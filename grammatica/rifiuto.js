68c5950724b318fd011fcc123e1e59142ddd32fd
eliaspallanzani <38375181+eliaspallanzani@users.noreply.github.com>
2023-02-21 17:41:42 +0100

    Update rifiuto.js

--- grammatica/rifiuto.js
+++ grammatica/rifiuto.js
@@ -1,47 +1,31 @@
 const grammar = `
 
-I ::= "title: rifiuto\
-"
-^ "author: fond elia spallanzani\
-"
-^ "language: italian\
-"
-^ "status: in corso\
-"
-^ "topic: mah\
-"
-^ "audience: tutti\
-"
-^ "comment: mah
-"
-^ "created: 24/04/2018"
-;
 
-S ::= Titolo \
+S ::= Titolo \\n
 
-Reprimenda \
+Reprimenda \\n
 
-Profezia \
+Profezia \\n
 
-Scherno \
+Scherno \\n
 
-Risposta \
+Risposta \\n
 
-ScambioParole \
+ScambioParole \\n
 
-ParlaScambiato \
+ParlaScambiato \\n
 
-ScambioLettere \
+ScambioLettere \\n
 
-ScambioTotale \
+ScambioTotale \\n
 
-Sconforto \
+Sconforto \\n
  
- Domanda \
+ Domanda \\n
 
-Opzione1 \
+Opzione1 \\n
 
-Opzione2 \
+Opzione2 \\n
 
 Opzione3;
 
@@ -52,9 +36,10 @@ Titolo ::= ("TEH INFINITE QUEST"
 )
 ;
 
-Reprimenda ::= {Saluto} {Aggettivo.Fav Epiteto.Fav Q | Aggettivo.Sfav Epiteto.Sfav} ^”!” ;
+Reprimenda ::= {Saluto} {Aggettivo.Fav Epiteto.Fav | Aggettivo.Sfav Epiteto.Sfav} ^”!” ;
 
 Saluto ::= (la peste a te | allora fanculo ^“!” | allora addio | che devo sentire ^”!” | mi deludi ) ^”;” esclama il buon "Tristano" ^”,”;
+
 Aggettivo ::= Fav: (coraggioso | impavido | scaltro | astuto)
 | Sfav: (diabolico | avido | schizofrenico | crudele);
 Epiteto ::= Fav: (eroe | avventuriero | cercatore di conoscenza)
@@ -70,9 +55,9 @@ e ti avvii (verso casa | alla taverna | al lupanare )
 in cerca di pi ^”&ugrave” (facili | redditizie | sensate) avventure;
 
 Risposta ::= lungo la strada incontri ((ArtInd.M Tizio.M) | (ArtInd.F Tizio.F)) dall'aria (spaventata | allarmata | oltraggiata | terrorizzata | incerta)
-e allora chiedi che (diavolo | cazzo | madonna) sta succedendo di nuovo ^”.” \
+e allora chiedi che (diavolo | cazzo | madonna) sta succedendo di nuovo ^”.” \\n
 
-la risposta suona pi ^”&ugrave” o meno ^”:” \
+la risposta suona pi ^”&ugrave” o meno ^”:” \\n
 ;
 
 ArtInd ::= (M: un | F: una);
@@ -81,15 +66,15 @@ Tizio ::= M: (contadino | bell'uomo | saltimbanco | perdigiorno | armigero )
 | F: (pastorella | donna | vecchia) (scarmigliata | dall'occhio febbricitante) ;
 
 ScambioParole ::=  {(signor mio | egregio signore | maledetto impiastro)} ^”,” {[ma]}
-{(non vedi | non ti accorgi | non capisci)} che {tutto} {si sta mischiando} ^”?”  \
+{(non vedi | non ti accorgi | non capisci)} che {tutto} {si sta mischiando} ^”?”  \\n
  ;
 
 ParlaScambiato ::= (follia | cimurro | da quando hanno chiuso i manicomi) ^”...” ”,”
 pensi ^”,” e rispondi per le rime ^”,”
 ma con (orrore | raccapriccio | incredulit ^”&agrave” | enorme sorpresa) senti la tua bocca
-(pronunciare | dire) queste parole ^”:” \
+(pronunciare | dire) queste parole ^”:” \\n
 
-“<<” {calmati} {non} {dire} {(sciocchezze | scemenze | assurdita')} V
+“<<” {calmati} {non} {dire} {(sciocchezze | scemenze | assurdit ^"&agrave")} ^","
 {cosa} {pensi} di {farmi} paura ^”?” “>>”
 ;
 
@@ -97,9 +82,10 @@ ScambioLettere ::= “<<” ma che succede “>>” “,” ti dici ^”,” e p
 paese pi ^”&ugrave” vicino ^”.” \\ intanto comincia a calare la notte e alzando lo sguardo non riconosci
 le costellazioni ^”.” “<<” (Segno.M Dovrebbe.M | Segno.F Dovrebbe.F) “>>” “,” ti dici “,” ma forse
 ricordi male ^”.” \\ nel frattempo sei arrivato al paese e gi ^”&agrave” scorgi l'insegna della taverna ^”,” che per ^”&ograve”
-adesso dice ^”:” \
+adesso dice ^”:” \\n
 
-“<<” "T"^Taverna^"A" “>>” ^”.” ;
+“<<” "T"^Taverna^"A" “>>” ^”.” \\n
+;
 
 Segno ::= M: (il \\ toro | il \\ capricorno | lo \\ scorpione | \\ auriga | il (piccolo | grande) \\ carro | \\ ofiuco )
 | F: ( la \\ vergine | \\ cassiopea | la \\ cintura di \\ ercole | la \\ scodella );
@@ -108,13 +94,13 @@ F: dovrebbe (essere pi ^”&ugrave” (a destra | bassa |lunga | larga) | avere 
 
 Taverna ::= {"A"}^{"V"}^{"E"}^{"R"}^{"N"};
 
-ScambioTotale ::= colto da un dubbio orrendo entri e ti rivolgi (al primo Tizio.M | alla prima Tizio.F) che vedi ^”:” \
+ScambioTotale ::= colto da un dubbio orrendo entri e ti rivolgi (al primo Tizio.M | alla prima Tizio.F) che vedi ^”:” \\n
  
-“<<” presto dimmi qualcosa ^”!” “>>” ^”,” gridi ^”,” “<<” qualcosa di sensato ^”!” \
+“<<” presto dimmi qualcosa ^”!” “>>” ^”,” gridi ^”,” “<<” qualcosa di sensato ^”!” ">>" \\n
 
-“<<” ParoleFuse “>>” “,” risponde ^”,” e aggiunge \
+“<<” ParoleFuse “>>” “,” risponde ^”,” e aggiunge \\n
 
-“<<” ParoleFuse “>>” ^”.” \
+“<<” ParoleFuse “>>” ^”.” \\n
 ;
 
 ParoleFuse ::= Lettere^Lettere^[Lettere] | ParoleFuse [^ParoleFuse];
@@ -124,8 +110,8 @@ Lettere ::= (++a| bf | ch | dy | ++e | fk | gw | hx | +i | ll | mq | nt | ++o | 
 
 Sconforto ::= allora mentre intorno a te i mobili e le sedie cominciano a tremare e a scambiarsi di posto
 capisci che il vecchio diceva la verit^”&agrave” ^”!” \\ il caos sta prendendo il sopravvento e ormai
-non ti resta che dire “<<” ParoleFuse “>>” ^”!” \
- ;
+non ti resta che dire “<<” ParoleFuse “>>” ^”!” \\n
+;
 
 
 Alvise := el (mago
@@ -134,7 +120,7 @@ Alvise := el (mago
 | pranoterapeuta )
 "Alvise" ;
 
-Domanda ::= (allora | ebbene | orbene) ^”,” che vuoi fare ^”?” \\ forse sei ancora in tempo per salvare il mondo ^”!” \
+Domanda ::= (allora | ebbene | orbene) ^”,” che vuoi fare ^”?” \\ forse sei ancora in tempo per salvare il mondo ^”!” \\n
 
 (ci hai ripensato | ti sei convinto | ti sei fatto persuaso) ^”?” ``n
 ora (sei (pronto a | impaziente di))
@@ -142,19 +128,19 @@ ora (sei (pronto a | impaziente di))
 (questa missione | quest'ordalia | il compito assegnato) ^”?” ;
 
 
-Opzione1 ::= "<a href=:rifiuto.html">" Pzione1 ^"</a>"
+Opzione1 ::= "<a href=.rifiuto.html">" Pzione1 ^"</a>"
 ;
 Pzione1 ::= (s ^”&iacute” ^”,” adesso "IO CREDO!" 
 | guarda ^”,” proprio perch ^”&egrave” vedo che ti sei sforzato
 | temo che se rifiutassi potresti commettere una sciocchezza)
 ;
-Opzione2 ::= "<a href=:rifiuto.html>" Pzione2 ^"</a>"
+Opzione2 ::= "<a href=.rifiuto.html>" Pzione2 ^"</a>"
 ;
 Pzione2 ::= (apprezzo il tentativo ma no ^”,” ho di meglio da fare
 | se c'era qualche minima speranza che continuassi questa indegna pantomima l'ha distrutta
 | ParoleFuse ^”!” )
 ;
-Opzione3 ::= "<a href=://www.facebook.com>" Pzione3 ^"</a>"
+Opzione3 ::= "<a href=https://www.facebook.com>" Pzione3 ^"</a>"
 ;
 Pzione3 ::= basta diocane^”!" | ma vai a cagare^”!" | squallido nerd io me ne vado
 ;
