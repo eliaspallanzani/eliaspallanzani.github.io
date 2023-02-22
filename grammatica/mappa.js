36de1f382110d5cc05483c9d8ab2f4d379739a7e
eliaspallanzani <38375181+eliaspallanzani@users.noreply.github.com>
2023-02-22 16:34:03 +0100

    Create mappa.js

---
+++ grammatica/mappa.js
@@ -0,0 +1,29 @@
+    const grammar = `
+
+I ::=   "title:    mappa\\n"
+      ^ "author:   Fondazione Elia Spallanzani\\n"
+      ^ "language: italian\\n"
+      ^ "status:   esperimento\\n"
+      ^ "topic:    misc\\n"
+      ^ "audience: tutti\\n"
+      ^ "created:  21/04/2018"
+;
+
+S ::= Riga "<br>"
+    Riga "<br>"
+Riga "<br>"
+Riga "<br>"
+;
+
+Riga ::= Car Car Car Car Car Car Car Car
+;
+
+Car :;= ( "&#176" 
+        | "&#177"
+        | "&#178"
+        | "&#219"
+        | "&#32"
+        )
+;
+
+`
